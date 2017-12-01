2df92e479ee654d76250a2dc59d1a588aeb4f5c4
AlexECX <33321263+AlexECX@users.noreply.github.com>
2017-12-01 17:45:23 -0500

    SDL_Delay

--- POO_SSS_projet/ProjetPOOWin32SDL/source/main.cpp
+++ POO_SSS_projet/ProjetPOOWin32SDL/source/main.cpp
@@ -23,21 +23,29 @@ typedef struct {
 	Joueur *PtrJoueur;
 } ThreadData;
 
+
+
+
 //----Thread Funct----------------------------------------------------------------------------
 
 int TestThread(void *ptr)
 {
 	ThreadData *tdata = (ThreadData*)ptr;
-	auto interval = std::chrono::nanoseconds(1000000000);
+	auto interval = std::chrono::nanoseconds(10000000);
 	auto before = std::chrono::high_resolution_clock::now();
+	auto before2 = std::chrono::high_resolution_clock::now();
 	int flying = 9000;
 	int H = 1;
 	while (flying)
 	{
-		tdata->PtrJoueur->UpdateTrajet(tdata->PtrJoueur->getCoordX() + H, tdata->PtrJoueur->getCoordY());
+		
 		if (std::chrono::high_resolution_clock::now() - before > interval) {
-			H *= -1;
+			tdata->PtrJoueur->UpdateTrajet(tdata->PtrJoueur->getCoordX() + H, tdata->PtrJoueur->getCoordY());
 			before = std::chrono::high_resolution_clock::now();
+		}
+		if (std::chrono::high_resolution_clock::now() - before2 > interval*50) {
+			H *= -1;
+			before2 = std::chrono::high_resolution_clock::now();
 		}
 	}
 
@@ -51,30 +59,16 @@ int main(int argc, char* args[])
 {
 	int X1 = 200;
 	int Y1 = 200;
+	auto Main_interval = std::chrono::nanoseconds(16666666); //16666666
+	auto Main_Timestamp = std::chrono::high_resolution_clock::now();
 	
 	//Thread initialise
 
 	SDL_Thread *thread;
 	int         threadReturnValue;
 
-	Joueur temp(joueur, republic, 50, 250, 1, 0, 0);
-	Joueur temp2(ennemis_simple, empire, 500, 250, 1, 0, 0);
-	ThreadData *message = (ThreadData*) malloc(sizeof(ThreadData));
-	message->PtrJoueur = &temp2;
 
-	/**/
-	//printf("\nSimple SDL_CreateThread test:");
 
-	//Simply create a thread 
-	thread = SDL_CreateThread(TestThread, "TestThread", message);
-
-	if (NULL == thread) {
-		printf("\nSDL_CreateThread failed: %s\n", SDL_GetError());
-	}
-	else {
-		SDL_WaitThread(thread, &threadReturnValue); //Wait for the thread to complete.
-		printf("\nThread returned value: %d", threadReturnValue);
-	}
 
 	
 
@@ -102,6 +96,17 @@ int main(int argc, char* args[])
 			SDL_Event e;
 
 			//////////////////////////
+			Joueur temp(joueur, republic, 50, 250, 1, 0, 0);
+			Joueur temp2(ennemis_simple, empire, 500, 250, 1, 0, 0);
+			ThreadData *message = (ThreadData*)malloc(sizeof(ThreadData));
+			message->PtrJoueur = &temp2;
+
+			/**/
+			//printf("\nSimple SDL_CreateThread test:");
+
+			//Simply create a thread 
+			thread = SDL_CreateThread(TestThread, "TestThread", message);
+			/////////////////////////////////
 
 			//While application is running
 			while (!quit)
@@ -115,26 +120,37 @@ int main(int argc, char* args[])
 						quit = true;
 					}
 				}
+			/*	auto tempTime = std::chrono::high_resolution_clock::now();
+				auto tempResult = tempTime - Main_Timestamp;
+				if (tempTime - Main_Timestamp < Main_interval) {
+					SDL_Delay(30);
+					Main_Timestamp = std::chrono::high_resolution_clock::now();
+				}
+				else {*/
+
+					SDL_Delay(16);
+
+					//Clear screen
+					SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+					SDL_RenderClear(gRenderer);
+
+					//Render background texture to screen
+					gBackgroundTexture.render(0, 0);
+
+					//Render Foo' to the screen
+
+					texture[temp.getCategorie()].render(temp.getCoordX(), temp.getCoordY());
+
+					texture[2].render(200, 300);
+
+					texture[temp2.getCategorie()].render(temp2.getCoordX(), temp2.getCoordY());
+
+					texture[3].render(350, 300);
+
+					//Update screen
+					SDL_RenderPresent(gRenderer);
+				//}
 				
-				//Clear screen
-				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
-				SDL_RenderClear(gRenderer);
-
-				//Render background texture to screen
-				gBackgroundTexture.render(0, 0);
-
-				//Render Foo' to the screen
-
-				texture[temp.getCategorie()].render(temp.getCoordX(), temp.getCoordY());
-
-				texture[2].render(200, 300);
-
-				texture[1].render(500, 250);
-
-				texture[3].render(350, 300);
-
-				//Update screen
-				SDL_RenderPresent(gRenderer);
 
 				//----Keyboard detect----
 
@@ -175,10 +191,12 @@ int main(int argc, char* args[])
 
 				}
 			}
+			SDL_WaitThread(thread, &threadReturnValue); //Wait for the thread to complete.
 		}
 	}
 
 	//Free resources and close SDL
+
 	close();
 
 	return 0;
